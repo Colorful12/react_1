4e249c91783244b4b556eb2ba5e31587ab730708
Colorful12 <cat37280@gmail.com>
2022-05-01 05:26:44 +0900

    make it possible to display all cuisines in the list

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
+    const cuisines = [
+      {id: 1, category: '',  name: 'ステーキ', price: 1000, content: '', pic: '../public/'},
+      {id: 2, category: '', name: 'ピザ', price: 700, content: '', pic: '../public/'},
+      {id: 3, category: '', name: 'お子様ランチ', price: 600, content: '', pic: '../public/'},
+      {id: 4, category: '', name: 'マリトッツォ', price: 350, content: '', pic: '../public/'},
+      {id: 5, category: '', name: '醤油ラーメン', price: 800, content: '', pic: '../public/'},
+      {id: 6, category: '', name: '豚骨ラーメン', price: 850, content: '', pic: '../public/'},
+    ];
+
+    const menu = cuisines.map((cuisine) =>
+    <div key={cuisine.id}>
+      <h3>{cuisine.name}</h3>
+      写真をいれる
+      <p>{cuisine.price} 円</p>
+      <p>{cuisine.content}</p>
+    </div>
+    );
     return (
-      <div>
-        <header>
-          <nav>
-
-          </nav>
-        </header>
         <div>
-          #リストにいれた要素たち(商品名, 画像, 値段 etc)をforでまわして画面に出力
+          {menu}
         </div>
-        <footer>
-
-        </footer>
-      </div>
     );
   }
 }
