532ce02d34f49ca7ebf649185b8c23f4a9e46303
Colorful12 <cat37280@gmail.com>
2022-05-01 22:54:18 +0900

    create header

---
+++ src/Head.js
@@ -0,0 +1,14 @@
+import './Head.css';
+import React from 'react';
+
+class Head extends React.Component {
+    render() {
+        return (
+            <header>
+                ヘッダー部分(色やテキストは仮)
+            </header>
+        );
+    }
+}
+
+export default Head;
