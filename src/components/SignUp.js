3d64b4ce119fe803ad7f99f73bf15628b7dadc7a
miurain <k1304118@kadai.jp>
2022-05-01 16:33:03 +0900

    signup1

---
+++ src/components/SignUp.js
@@ -0,0 +1,27 @@
+const SignUp = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('登録');
+  };
+
+  return (
+    <div>
+      <h1>ユーザ登録</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input name="email" type="email" placeholder="email" />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
