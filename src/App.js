68155ee09eae0a7676f81733e52ec46f6d0dfbfd
miurain <k1304118@kadai.jp>
2022-05-05 13:30:02 +0900

    loginfunction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import Home from './components/Home';
 import SignUp from './components/SignUp';
+import Login from './components/Login';
+import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
   return (
-    <div style={{ margin: '2em' }}>
-      <SignUp />
-    </div>
+    <AuthProvider>
+      <div style={{ margin: '2em' }}>
+        <BrowserRouter>
+          <PrivateRoute exact path="/" component={Home} />
+          <PublicRoute path="/signup" component={SignUp} />
+          <PublicRoute path="/login" component={Login} />
+        </BrowserRouter>
+      </div>
+    </AuthProvider>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,29 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+
+
+
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+//const firebaseConfig = {
+  //apiKey: "AIzaSyAOloBdXW9vjg51AZx4ifLksV1uNbe4Yh8",
+  //authDomain: "react-reservation-97e22.firebaseapp.com",
+  //projectId: "react-reservation-97e22",
+  //storageBucket: "react-reservation-97e22.appspot.com",
+  //messagingSenderId: "787719171722",
+  //appId: "1:787719171722:web:63ff8d8397951a9011c773"
+//};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+
+//export const auth = firebase.auth();
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,4 +36,8 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+//export const provider = new firebase.auth.GoogleAuthProvider(); 
+
 export const auth = firebase.auth();
+
+//export default firebase;
