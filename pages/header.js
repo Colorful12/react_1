d2fb5d257b6a2c68f559ebafcf94bdd4593824fe
Colorful12 <cat37280@gmail.com>
2022-05-08 20:43:42 +0900

    make .css related to logout

--- pages/header.js
+++ pages/header.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { getAuth, signOut } from "firebase/auth";
 
 class Head extends React.Component {
     render() {
+        const auth = getAuth();
+        const handleLogout = () => {
+            signOut(auth).then(() => {
+            window.open(" http://localhost:3000/login")
+            })
+        };
         return (
             <header>
                 <div class="main_img">
@@ -11,6 +18,7 @@ class Head extends React.Component {
                     React Practice
                 </div>
                 <div class="inner_nav">
+                    <button onClick={handleLogout}>ログアウト</button>
                     <a href="" class="tel">お問い合わせ</a>
                     <a href="" class="acs">アクセス</a>
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -3,28 +3,11 @@ import Menu from './menu'
 import {useHistory } from 'react-router-dom';
 
 
-import FirebaseApp from './firebase'
-import { getAuth, signOut } from "firebase/auth";
-
-export default function Home() {
-
-  const auth = getAuth();
-  const handleLogout = () => {
-    signOut(auth).then(() => {
-      window.open(" http://localhost:3000/login")
-    })
-    
-  };
-  
+export default function Home() {  
   return (
     <div>
-      
       <Header/>
-      <button onClick={handleLogout}>ログアウト</button>
       <Menu/>
-      
-      
-     
     </div>
     
   )
